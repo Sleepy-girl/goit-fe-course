8cc1b83d737d1d0ce9c7debbbc2723ca96f5c7a4
nyuta <pillowforneedles@gmail.com>
2020-05-12 20:55:55 +0300

    js module-01 (task 1-4)

---
+++ javascript/module-01/task-04.js
@@ -0,0 +1,12 @@
+const credits = 23580;
+const pricePerDroid = 3000;
+
+const totalPrice = prompt ('Какое количество дроидов Вам необходимо?');
+
+if (totalPrice === null) {
+  alert ('Отменено пользователем!');
+} else if (totalPrice * pricePerDroid > credits) {
+  alert ('Недостаточно средств на счету!');
+} else if (totalPrice * pricePerDroid <= credits) {
+  alert ('Вы купили ' + totalPrice + ' дроидов, на счету осталось ' + (credits - totalPrice * pricePerDroid) + ' кредитов.');
+}
